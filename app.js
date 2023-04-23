6e7cf19ffd39ee14ea4b6fe1cbca2e1ea5d339fb
ParkBrianJunSoo <junparkstar@gmail.com>
2023-04-24 02:01:31 +0900

    nodemon 추가

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 3005;
 
 const goodsRouter = require('./routes/goods'); // './' = 상대 경로를 나타내는 것
 // routes라는 폴더에서 goods.js 파일을 찾음
